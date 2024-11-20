de7f100cb9a476988e8177f3043ebf27062f9e34
Shubham Prajapati <shubhamprajapati032@gmail.com>
2024-11-20 15:08:12 +0530

    "Added validation for environment variables, updated dependencies, and modified config.go and type.go files."

--- config/config.go
+++ config/config.go
@@ -1,8 +1,10 @@
 package config
 
 import (
+	"fmt"
 	"sync"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
@@ -10,21 +12,28 @@ import (
 var (
 	once            sync.Once
 	config_Instance *Config
+	env             Enviroment
 )
 
 func GetConfig() *Config {
 	once.Do(func() {
-		var env Enviroment
 		viper.SetConfigFile(".env")
-		viper.ReadInConfig()
+		if err := viper.ReadInConfig(); err != nil {
+			logrus.Fatalf("Failed to load the configuration file from disk and key/value stores, searching in one of the defined paths : %s\n", err)
+		}
 
 		if err := viper.ReadInConfig(); err != nil {
-			logrus.Errorf("Failed to load enviroment : %s", err)
+			logrus.Fatalf("Failed to load enviroment : %s\n", err)
 		}
 
 		if err := viper.Unmarshal(&env); err != nil {
-			logrus.Errorf("Failed to decode the enviroment : %s", err)
+			logrus.Fatalf("Failed to decode the enviroment : %s\n", err)
 		}
+
+		if err := validator.New().Struct(&env); err != nil {
+			logrus.Fatalf("Failed to validate the enviroment : %s\n", err)
+		}
+		fmt.Printf("%+v\n", env)
 	})
 	return config_Instance
 }
--- config/type.go
+++ config/type.go
@@ -20,9 +20,10 @@ type (
 )
 
 type Enviroment struct {
-	DB_HOST     string `mapstructure:"DB_HOST"`
-	DB_PORT     int    `mapstructure:"DB_PORT"`
-	DB_DATABASE string `mapstructure:"DB_DATABASE"`
-	DB_USERNAME string `mapstructure:"DB_USERNAME"`
-	DB_PASSWORD string `mapstructure:"DB_PASSWORD"`
+	DB_HOST     string `mapstructure:"DB_HOST" validate:"required"`
+	DB_PORT     int    `mapstructure:"DB_PORT" validate:"required"`
+	DB_DATABASE string `mapstructure:"DB_DATABASE" validate:"required"`
+	DB_USERNAME string `mapstructure:"DB_USERNAME" validate:"required"`
+	DB_PASSWORD string `mapstructure:"DB_PASSWORD" validate:"required"`
+	SERVER_PORT int    `mapstructure:"SERVER_PORT" validate:"required"`
 }
